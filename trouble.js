89c6793a89de1226fc19a0d7b09319ab470d7878
Karan Modi <kpmodi@my.waketech.edu>
2025-01-24 12:31:10 -0500

    Added stuff

---
+++ trouble.js
@@ -0,0 +1,46 @@
+let v1 = 3;
+let v2 = 4;
+alert("3 times 4" + " equals " + v1 * v2); // Remove comment and add quotes to equals
+
+ // This sets the general styling for the divs within the section tag using .style
+            let section = document.getElementById("s1") // Remove the S from elements
+            section.style.width = "50%"
+            section.style.textAlign = "center"
+            section.style.fontFamily = "arial, serif"
+            section.style.fontWeight = "bold" // spell style correctly
+            section.style.fontStyle = "italic"
+            section.style.fontSize = "20px"
+
+            // Sets the customization for each individual color/div tag
+			//.innerHTML is used to add content within each div tag.
+			//.style.backgroundColor is used to assign a different background color to each div
+			
+            // RED
+            const red = document.getElementById("red") // Capitalize Element
+            red.innerHTML = "RED"
+            red.style.backgroundColor = "red"
+
+            // ORANGE
+            const orange = document.getElementById("orange")
+            orange.innerHTML = "ORANGE" //make HTML Capitalized
+            orange.style.backgroundColor = "orange"
+
+            // YELLOW
+            const yellow = document.getElementById("yellow")
+            yellow.innerHTML = "YELLOW"
+            yellow.style.backgroundColor = "yellow"
+
+            // GREEN
+            const purple = document.getElementById("green")
+            green.innerHTML = "GREEN"
+            green.style.backgroundColor = "green"
+
+            // INDIGO
+            const indigo = document.getElementById("indigo")
+            indigo.innerHTML = "INDIGO"
+            indigo.style.backgroundColor = "indigo"
+
+            // PURPLE
+            const violet = document.getElementById("violet")
+            violet.innerHTML = "VIOLET"
+            violet.style.backgroundColor = "violet"
